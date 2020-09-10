cf397adf3c943c70479d5be86da7a978edecae1b
vishalbuha <buhavishal1@gmail.com>
2020-09-10 18:49:05 +0530

    add card detail

--- Gradgap/View/Profile/AddNewCardVC.swift
+++ Gradgap/View/Profile/AddNewCardVC.swift
@@ -37,6 +37,9 @@ class AddNewCardVC: UIViewController {
     //MARK: - configUI
     func configUI() {
         successBackView.isHidden = true
+        
+        cardNumberTxt.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    
     }
       
     //MARK: - Button Click
@@ -45,14 +48,28 @@ class AddNewCardVC: UIViewController {
     }
 
     @IBAction func clickToMonthSelect(_ sender: Any) {
-        
+        DatePickerManager.shared.showPicker(title: "Select Month", selected: "", strings: monthArr) { [weak self](month, index, success) in
+            if month != nil {
+                self?.monthTxt.text = month
+            }
+            self?.view.endEditing(true)
+        }
     }
     
     @IBAction func clickToyearSelect(_ sender: Any) {
-        
+//        DatePickerManager.shared.showPicker(title: "Select Year", selected: "", strings: graduationYear) { [weak self](school, index, success) in
+//            if school != nil {
+//                self?.startingSchoolTxt.text = school
+//            }
+//            self?.view.endEditing(true)
+//        }
     }
     
     @IBAction func clickToSave(_ sender: Any) {
+        
+        
+        
+        
         successBackView.isHidden = false
         displaySubViewtoParentView(self.view, subview: successBackView)
     }
@@ -61,6 +78,63 @@ class AddNewCardVC: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if(textField == cardNumberTxt){
+            cardNumberTxt.text = showCardNumberFormattedStr(cardNumberTxt.text!, isRedacted: false)
+        }
+    }
+        
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if(string == ""){
+            return true
+        }
+        guard let text = textField.text else { return true }
+        let newLength = text.count + string.count - range.length
+        
+        if(textField == cardNumberTxt){
+            var maxLength:Int = 0
+            maxLength = CARD_NUMBER_CHAR + CARD_NUMBER_DASH_CHAR
+            let str:String = cardNumberTxt.text!
+            return str.count < maxLength
+        }
+        else if textField == cvvTxt
+        {
+            return newLength <= 3
+        }
+        return true
+    }
+    
+    func showCardNumberFormattedStr(_ str:String, isRedacted:Bool = true) -> String{
+        
+        let tempStr:String = sendDetailByRemovingChar(sendDetailByRemovingChar(str, char:"-"), char: " ")
+        var retStr:String = ""
+        for i in 0..<tempStr.count{
+            if(i == 4 || i == 8 || i == 12){
+                retStr += "-"
+            }
+            retStr += tempStr[i]
+        }
+        if(isRedacted){
+            var arr:[String] = retStr.components(separatedBy: "-")
+            for i in 0..<arr.count{
+                if(i == 1 || i == 2){
+                    arr[i] = "xxxx"
+                }
+            }
+            retStr = arr.joined(separator: "-")
+        }
+        return retStr
+    }
+    
+    func sendDetailByRemovingChar(_ str:String, char:String = " ") -> String{
+        let regExp :String = char + "\n\t\r"
+        return String(str.filter { !(regExp.contains($0))})
+    }
+    
+//    func getYearArr() {
+//        let date = getDateStringFromDateString(strDate: Date(), formate: "yyyy")
+//        
+//    }
     
     deinit {
         log.success("AddNewCardVC Memory deallocated!")/
